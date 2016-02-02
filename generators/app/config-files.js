0f5199061afca04fe4068c0df34e874431935e7a
joneqian <qian.qing@aliyun.com>
2016-02-02 17:20:38 +0800

    add file

---
+++ generators/app/config-files.js
@@ -0,0 +1,28 @@
+/**
+ * @author qianqing
+ * @create by 16-2-2
+ * @description
+ */
+/**
+ * config-files.js
+ * Sets up the config files templates for the generator
+ */
+module.exports = function() {
+  this.template('_package.json', 'package.json');
+
+  /* #bower */
+  this.template('_bower.json', 'bower.json');
+  this.copy('bowerrc', '.bowerrc');
+
+  /* #travis */
+  this.copy('travis.yml', '.travis.yml');
+
+  /* #jshint */
+  this.copy('jshintrc', '.jshintrc');
+
+  /* #git */
+  this.copy('gitignore', '.gitignore');
+
+  /* #editor */
+  this.copy('editorconfig', '.editorconfig');
+}
