47ae18e7e84bd052a07a15ac5b63103337a5b313
joneqian <qian.qing@aliyun.com>
2016-02-15 13:23:31 +0800

    Handlebars

--- generators/app/templates/app.js
+++ generators/app/templates/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -8,14 +8,14 @@ var compression = require('compression');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
 app.use(compression());
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
